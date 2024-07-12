87135d1270df3685f1834d722785f49df2148197
anudin0899 <anudhinkk@gmail.com>
2024-07-12 18:24:46 +0530

    base url changed

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,7 +3,7 @@ const { default: axios } = require("axios")
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY
 
 const axiosCLient = axios.create({
-    baseURL: 'http://localhost:1337/api',
+    baseURL: 'https://doctor-booking-admin-bystrapi.onrender.com/api',
     headers: {
         'Authorization': `Bearer ${API_KEY}`
     }
